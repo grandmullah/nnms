c247ac86354344de7448ce551301c3fc295a4930
grandmullah <collinsbiegon9@gmail.com>
2023-03-07 18:35:47 +0300

    fix

--- src/components/Footer/Index.tsx
+++ src/components/Footer/Index.tsx
@@ -117,7 +117,7 @@ export function FooterLinks() {
         "links": [
           {
             "label": "Hospital Management",
-            "link": "/hospital"
+            "link": "/nnms/hospital"
           },
           {
             "label": " County Administration",
